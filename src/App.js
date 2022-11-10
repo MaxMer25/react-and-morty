37e2c8958ffa1e3ed551dd60359d38c08f54576a
Maximilian Mergenthal <max-mergenthal@gmx.de>
2022-11-10 14:59:01 +0100

    htmlMarkup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <Header />
+      <header>
+        <h1>React and Morty!</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Rick</a>
+            </li>
+            <li>
+              <a href="">Morty</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="Card">
+          <img
+            src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+            alt="Morty"
+          />
+          <p>Morty Smith</p>
+        </div>
+      </header>
     </div>
   );
 }
